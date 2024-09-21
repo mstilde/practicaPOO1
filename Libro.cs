7cbbcceaac755b07b5477bcd5a7cc6e6bcdc4c16
mstilde <matiasemanuelbuttari@hotmail.com>
2024-09-21 10:28:43 -0300

    first commit

---
+++ Libro.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComIT.Console
+{
+    public class Libro
+    {
+        private string titulo;
+        private string autor;
+        private int copias;
+        private int copiasEnPrestamo;
+
+        public Libro()
+        {
+            titulo = "N/A";
+            autor = "N/A";
+            copias = 0;
+            copiasEnPrestamo = 0;
+        }
+
+        public Libro(string ti, string au, int co, int coPr)
+        {
+            titulo = ti;
+            autor = au;
+            copias = co;
+            copiasEnPrestamo = coPr;
+        }
+
+        public bool Prestar()
+        {
+            if (copias > 1)
+            {
+                copias--;
+                copiasEnPrestamo++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Devolver()
+        {
+            if (copiasEnPrestamo > 1) 
+            {
+                copiasEnPrestamo--;
+                copias++;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void Datos()
+        {
+            System.Console.WriteLine("Titulo:   " + titulo);
+            System.Console.WriteLine("Autor:   " + autor);
+            System.Console.WriteLine("Copias:   " + copias);
+            System.Console.WriteLine("Prestamos:   " + copiasEnPrestamo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+// EJERCICIOS POO 1
+
+using ComIT.Console;
+
+Libro libro1 = new();
+
+Libro libro2 = new("El Color Que Cayó del Cielo", "H.P. Lovecraft", 5, 2);
+
+// Verificar datos del libro 1 generados correctamente
+libro1.Datos();
+
+// Verificar datos del libro 2 generados correctamente
+libro2.Datos();
+
+// Verificar datos del libro 2 actualizados correctamente
+libro2.Prestar();
+libro2.Datos();
+
+// Verificar datos del libro 2 actualizados correctamente
+libro2.Devolver();
+libro2.Datos();
